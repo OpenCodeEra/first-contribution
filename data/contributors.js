ce04dcee80ead7ec5b1a575b2ba659fc1b979839
Rahul Prasad <rahulprasad.panagarh@gmail.com>
2024-09-24 10:58:47 +0530

    Rahul Prasad

--- data/contributors.js
+++ data/contributors.js
@@ -1,74 +1,82 @@
 const contributors = [
   // add your details here
   {
-    name: "Adarsh", 
-    skill: "Web Cloud ML", 
-    linkedin: "https://www.linkedin.com/in/adarsh8304/", 
+    name: "Rahul Prasad",
+    skill: "Python | Html | Css",
+    linkedin: "https://www.linkedin.com/in/rahul-prasad-78b260324/",
+    github: "https://github.com/rahul-prasad04",
+    gender: "male"
+  },
+
+  {
+    name: "Adarsh",
+    skill: "Web Cloud ML",
+    linkedin: "https://www.linkedin.com/in/adarsh8304/",
     github: "https://github.com/AKACHI-4",
     gender: "male"
   },
   {
-    name: "Adarsh Rawat", 
-    skill: "Web Dev UI/UX  ", 
-    linkedin: "https://www.linkedin.com/in/adarsh-rawat/", 
+    name: "Adarsh Rawat",
+    skill: "Web Dev UI/UX  ",
+    linkedin: "https://www.linkedin.com/in/adarsh-rawat/",
     github: "https://github.com/AdarshRawat1",
     gender: "male"
   },
   {
-    name: "Harsh Raj", 
-    skill: "Full-stack development", 
+    name: "Harsh Raj",
+    skill: "Full-stack development",
     linkedin: "https://www.linkedin.com/in/harsh-raj-1b6638258/",
     github: "https://github.com/harsh661",
     gender: "male"
   },
   {
-    name: "Ratan Gulati", 
-    skill: "Cloud, DevOps, JAVA", 
-    linkedin: "https://www.linkedin.com/in/ratangulati/", 
+    name: "Ratan Gulati",
+    skill: "Cloud, DevOps, JAVA",
+    linkedin: "https://www.linkedin.com/in/ratangulati/",
     github: "https://github.com/Ratangulati",
     gender: "male"
   },
   {
-    name: "Ishan Joshi", 
-    skill: "Web Developer", 
-    linkedin: "https://www.linkedin.com/in/ishan-joshi-98b3a2217", 
+    name: "Ishan Joshi",
+    skill: "Web Developer",
+    linkedin: "https://www.linkedin.com/in/ishan-joshi-98b3a2217",
     github: "https://github.com/Taejin101",
     gender: "male"
-  }, 
-  {
-    name : "Rakshit Mehta",
-    skill : "React, Typescript, Node JS",
-    linkedin : "https://www.linkedin.com/in/rakshit-mehta-360570284/",
-    github : "https://github.com/rakshit0960",
-    gender : "male"
   },
   {
-    name : "Naitik Singhal",
-    skill : "Cloud, C++",
-    linkedin : "https://www.linkedin.com/in/naitik-singhal-b52b46218/",
-    github : "https://github.com/Lucifer-Overridden",
-    gender : "male"
+    name: "Rakshit Mehta",
+    skill: "React, Typescript, Node JS",
+    linkedin: "https://www.linkedin.com/in/rakshit-mehta-360570284/",
+    github: "https://github.com/rakshit0960",
+    gender: "male"
   },
   {
-    name : "Vijay Gunwant",
-    skill : "Web | App | Cloud",
-    linkedin : "www.linkedin.com/in/vijay-gunwant",
-    github : "https://github.com/Vijay-Gunwant",
-    gender : "male"
+    name: "Naitik Singhal",
+    skill: "Cloud, C++",
+    linkedin: "https://www.linkedin.com/in/naitik-singhal-b52b46218/",
+    github: "https://github.com/Lucifer-Overridden",
+    gender: "male"
+  },
+  {
+    name: "Vijay Gunwant",
+    skill: "Web | App | Cloud",
+    linkedin: "www.linkedin.com/in/vijay-gunwant",
+    github: "https://github.com/Vijay-Gunwant",
+    gender: "male"
   },
   {
     name: "Pruthviraj Ilegaonkar",
     skill: "Web | DevOps",
     linkedin: "https://www.linkedin.com/in/pruthviraj-ilegaonkar-6b5237274/",
     github: "https://github.com/CodeTorso",
-    gender : "male"
+    gender: "male"
   },
   {
-    name : "Ayush Raj",
-    skill : "FrontEnd & Cloud",
-    linkedin : "https://in.linkedin.com/in/ayush-raj-bhatt-0685051a9",
-    github : "www.github.com/ayush41",
-    gender : "male"
-},
+    name: "Ayush Raj",
+    skill: "FrontEnd & Cloud",
+    linkedin: "https://in.linkedin.com/in/ayush-raj-bhatt-0685051a9",
+    github: "www.github.com/ayush41",
+    gender: "male"
+  },
 ]
 export default contributors;
